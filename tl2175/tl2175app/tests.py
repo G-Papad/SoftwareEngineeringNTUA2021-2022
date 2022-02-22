140d966b2fdb639cbe57ea1959ed1abfbf77965b
G-Papad <geopapadoulis@gmail.com>
2022-02-22 02:28:12 +0200

    firt try testing cli

--- tl2175/tl2175app/tests.py
+++ tl2175/tl2175app/tests.py
@@ -1,3 +1,12 @@
+from io import StringIO
+from django.core.management import call_command
 from django.test import TestCase
 
 # Create your tests here.
+
+class PassesCostTest (TestCase):
+    def test_command_output(self):
+        out =StringIO()
+        call_command('passescost', '--op1=KO', '--op2=OO','--datefrom=20201001', '--dateto=20201031', '--format=json', stdout=out)
+        print("out: " + out.getvalue() + " here")
+        self.assertIn('13,80', out.getvalue())
