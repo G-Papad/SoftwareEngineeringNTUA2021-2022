510d1625c69a39661d069c0867ed0262c4a88879
Christina Proestaki <chri.proe@gmail.com>
2022-02-23 13:02:42 +0200

    cli testing - all

--- tl2175/tl2175app/management/commands/passescost.py
+++ tl2175/tl2175app/management/commands/passescost.py
@@ -25,11 +25,13 @@ class Command(BaseCommand):
 
         provider1 = Provider.objects.filter(providerAbbr=op1)
         provider2 = Provider.objects.filter(providerAbbr=op2)
+        """
         if (not provider1.exists()) or (not provider2.exists()):
             print("Invalid arguments: Provider does not exist", file=self.stdout)
             return
+        """
         if(df > dt):
-            print("Invlide arguments: date_from > date_to", file=self.stdout)
+            print("Invalid arguments: date_from > date_to", file=self.stdout)
             return
         name_from = df
         name_to = dt
@@ -39,7 +41,9 @@ class Command(BaseCommand):
             df = datetime.strptime(df+"000000", "%Y%m%d%H%M%S").strftime(
                 "%Y-%m-%d %H:%M:%S")
         except:
-            print("Wrong DateTime Format")
+            print("Wrong DateTime Format", file = self.stdout)
+            return
+
 
         passes = Passes.objects.filter(passes_fk1__station_fk__providerAbbr=op1).filter(passes_fk2__vehicle_fk1__providerAbbr=op2).exclude(timestamp__gte=dt).filter(timestamp__gte=df)
         provider = Provider.objects.filter(providerAbbr=op1)
@@ -48,12 +52,12 @@ class Command(BaseCommand):
         response = requests.get(url).json()
         if format == 'json':
             print(response, file=self.stdout)
-            name1 = "tl2175app/management/commands/results/json/PassesCost" + op1 + "_"  + op2 + '_' + name_from + "_" + name_to + ".json"
+            name1 = "tl2175app/management/commands/results/json/PassesCost_" + op1 + "_"  + op2 + '_' + name_from + "_" + name_to + ".json"
             if savejson == 'yes':
                 with open(name1, 'w') as f:
                     json.dump(response, f)
         else:
-            name = "tl2175app/management/commands/results/csv/PassesCost" + op1 + "_" + op2 + '_' + name_from + "_" + name_to + ".csv"
+            name = "tl2175app/management/commands/results/csv/PassesCost_" + op1 + "_" + op2 + '_' + name_from + "_" + name_to + ".csv"
             data_file = open(name, 'w', newline = '')
             data = response
             if data == []:
--- tl2175/tl2175app/test_cli.py
+++ tl2175/tl2175app/test_cli.py
@@ -13,16 +13,17 @@ import requests
 import os.path
 from os import path
 
-class PassesCostTest (TestCase):
-    @classmethod
-    def setUpTestData(self):
-        call_command('loaddata', 'db.json', verbosity=0)
-    def test_command_output(self):
-        out =StringIO()
-        call_command('passescost', op1='OO', op2='KO', datefrom='20201001', dateto='20201031', format='json', stdout=out)
-        self.assertIn('13.8', out.getvalue())
 
-# class PassesAnalysisTest(TestCase):
+#class PassesCostTest (TestCase):
+    #@classmethod
+    #def setUpTestData(self):
+        #call_command('loaddata', 'db.json', verbosity=0)
+    #def test_command_output(self):
+        #out =StringIO()
+        #call_command('passescost', op1='OO', op2='KO', datefrom='20201001', dateto='20201031', format='json', stdout=out)
+        #self.assertIn('13.8', out.getvalue())
+
+class PassesAnalysisTest(TestCase):
     def do_command(self, *args, **options):
         with StringIO() as f:
             call_command(*args, **options, stdout = f)
@@ -34,17 +35,13 @@ class PassesCostTest (TestCase):
     def test_passesanalysis_wrong_dateformat(self):
         text = self.do_command('passesanalysis', op1 = 'KO', op2 = 'AO', datefrom = '2020/04/04', dateto = '20350404')
         self.assertEqual('Wrong DateTime Format\n\n', text)
-
-        #fix this when connect to db
-
-        """
+    """
     def test_passesanalysis_wrong_ops(self):
         text = self.do_command('passesanalysis', op1 = 'AJ', op2 = 'AO', datefrom = '20190404', dateto = '20200404')
         self.assertEqual('Invalid arguments: Provider does not exist\n\n', text)
-        """
-
+    """
     def test_passesanalysis_savejson(self):
-        text = self.do_command('passesanalysis', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404', savejson = "yes")
+        self.do_command('passesanalysis', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404', savejson = "yes")
         if (not path.exists("tl2175app/management/commands/results/json/PassesAnalysis_KO_AO_20200320_20200404.json")):
             self.assertEqual(0,1)
 
@@ -58,16 +55,144 @@ class PassesCostTest (TestCase):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.assertEqual("""{'op1_ID': 'KO', 'op2_ID': 'AO', 'RequestTimeStamp': '"""+timestamp+"""', 'PeriodFrom': '2020-03-20 00:00:00', 'PeriodTo': '2020-04-04 00:00:00', 'NumberOfPasses': 2, 'PassesList': [{'passid': 'CNQ8711527', 'timestamp': '2020-03-26T20:59:00', 'charge': '2.00', 'stationRef': 'kentriki_odos tolls station 03', 'vehicleRef': 'OC94ASJ72024', 'PassIndex': 1}, {'passid': 'NCF9224902', 'timestamp': '2020-03-24T17:25:00', 'charge': '1.00', 'stationRef': 'kentriki_odos tolls station 09', 'vehicleRef': 'HT62RDI04611', 'PassIndex': 2}]}\n\n""", text)
 
+class PassesPerStationTest(TestCase):
+    def do_command(self, *args, **options):
+        with StringIO() as f:
+            call_command(*args, **options, stdout = f)
+            return f.getvalue()
+    def test_passesperstation_wrong_dates(self):
+        text = self.do_command('passesperstation', station = 'KO01', datefrom = '20200404', dateto = '20190404')
+        self.assertEqual('Invalid arguments: date_from > date_to\n\n', text)
+
+    def test_passesperstation_wrong_dateformat(self):
+        text = self.do_command('passesperstation', station = 'KO01', datefrom = '2020/04/04', dateto = '20350404')
+        self.assertEqual('Wrong DateTime Format\n\n', text)
+
+        """
+    def test_passesperstation_wrong_ops(self):
+        text = self.do_command('passesperstation', station = 'AJ', datefrom = '20190404', dateto = '20200404')
+        self.assertEqual('Invalid arguments: Provider does not exist\n\n', text)
+        """
+    def test_passesperstation_savejson(self):
+        text = self.do_command('passesperstation', station = 'KO01', datefrom = '20200320', dateto = '20200404', savejson = "yes")
+        if (not path.exists("tl2175app/management/commands/results/json/PassesPerStation_KO01_20200320_20200404.json")):
+            self.assertEqual(0,1)
+
+    def test_passesperstation_csv(self):
+        text = self.do_command('passesperstation', station = 'KO01', datefrom = '20200320', dateto = '20200404', format = 'yes')
+        if (not path.exists("tl2175app/management/commands/results/csv/PassesPerStation_KO01_20200320_20200404.csv")):
+            self.assertEqual(0,1)
+
+    def test_passesperstation_test01(self):
+        text = self.do_command('passesperstation', station = 'KO01', datefrom = '20200320', dateto = '20200404')
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.assertEqual("""{'Station': 'KO01', 'StationOperator': 'kentriki_odos', 'RequestTimeStamp': '""" + timestamp + """', 'PeriodFrom': '2020-03-20 00:00:00', 'PeriodTo': '2020-04-04 00:00:00', 'NumberOfPasses': 6, 'PassesList': [{'passid': 'CLJ2137833', 'timestamp': '2020-03-20T21:53:00', 'charge': '2.50', 'vehicleRef': 'IX01MVL33676', 'pass_type': 'home', 'PassIndex': 1, 'TagProvider': 'kentriki_odos'}, {'passid': 'CII8631766', 'timestamp': '2020-03-27T07:51:00', 'charge': '2.80', 'vehicleRef': 'UO75YNW62238', 'pass_type': 'home', 'PassIndex': 2, 'TagProvider': 'kentriki_odos'}, {'passid': 'CHO0835399', 'timestamp': '2020-03-28T09:40:00', 'charge': '2.80', 'vehicleRef': 'ED51EWW52190', 'pass_type': 'home', 'PassIndex': 3, 'TagProvider': 'kentriki_odos'}, {'passid': 'MLU6406429', 'timestamp': '2020-03-30T16:04:00', 'charge': '2.20', 'vehicleRef': 'HW75BKT77773', 'pass_type': 'home', 'PassIndex': 4, 'TagProvider': 'kentriki_odos'}, {'passid': 'CDC9700930', 'timestamp': '2020-04-02T02:50:00', 'charge': '2.80', 'vehicleRef': 'YH66OKD41942', 'pass_type': 'home', 'PassIndex': 5, 'TagProvider': 'kentriki_odos'}, {'passid': 'POV0246876', 'timestamp': '2020-04-02T08:50:00', 'charge': '1.00', 'vehicleRef': 'WY00MLL63827', 'pass_type': 'home', 'PassIndex': 6, 'TagProvider': 'kentriki_odos'}]}\n\n""", text)
+
         #add more tests for other instances
 
-# class HealthCheckTest(TestCase):
-#     def do_command(self, *args, **options):
-#         with StringIO() as f:
-#             call_command(*args, stdout = f)
-#             return f.getvalue()
+class PassesCostTest(TestCase):
+    def do_command(self, *args, **options):
+        with StringIO() as f:
+            call_command(*args, **options, stdout = f)
+            return f.getvalue()
+    def test_passescost_wrong_dates(self):
+        text = self.do_command('passescost', op1 = 'KO', op2 = 'AO', datefrom = '20200404', dateto = '20190404')
+        self.assertEqual('Invalid arguments: date_from > date_to\n\n', text)
+
+    def test_passescost_wrong_dateformat(self):
+        text = self.do_command('passescost', op1 = 'KO', op2 = 'AO', datefrom = '2020/04/04', dateto = '20350404')
+        self.assertEqual('Wrong DateTime Format\n\n', text)
+    """
+    def test_passescost_wrong_ops(self):
+        text = self.do_command('passescost', op1 = 'AJ', op2 = 'AO', datefrom = '20190404', dateto = '20200404')
+        self.assertEqual('Invalid arguments: Provider does not exist\n\n', text)
+    """
+    def test_passescost_savejson(self):
+        self.do_command('passescost', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404', savejson = "yes")
+        if (not path.exists("tl2175app/management/commands/results/json/PassesCost_KO_AO_20200320_20200404.json")):
+            self.assertEqual(0,1)
+
+    def test_passescost_csv(self):
+        text = self.do_command('passescost', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404', format = 'yes')
+        if (not path.exists("tl2175app/management/commands/results/csv/PassesCost_KO_AO_20200320_20200404.csv")):
+            self.assertEqual(0,1)
+
+    def test_passescost_test01(self):
+        text = self.do_command('passescost', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404')
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.assertEqual("""{'Operator1': 'KO', 'Operator2': 'AO', 'RequestTimestamp': '""" + timestamp + """', 'PeriodFrom': '2020-03-20 00:00:00', 'PeriodTo': '2020-04-04 00:00:00', 'NumberOfPasses': 2, 'PassesCost': 3.0}\n\n""", text)
+
+
+class ChargesByTest(TestCase):
+    def do_command(self, *args, **options):
+        with StringIO() as f:
+            call_command(*args, **options, stdout = f)
+            return f.getvalue()
+    def test_chargesby_wrong_dates(self):
+        text = self.do_command('chargesby', op1 = 'KO', datefrom = '20200404', dateto = '20190404')
+        self.assertEqual('Invalid arguments: date_from > date_to\n\n', text)
+
+    def test_chargesby_wrong_dateformat(self):
+        text = self.do_command('chargesby', op1 = 'KO', datefrom = '2020/04/04', dateto = '20350404')
+        self.assertEqual('Wrong DateTime Format\n\n', text)
+
+        """
+    def test_chargesby_wrong_ops(self):
+        text = self.do_command('chargesby', op1 = 'AJ', datefrom = '20190404', dateto = '20200404')
+        self.assertEqual('Invalid arguments: Provider does not exist\n\n', text)
+        """
+    def test_chargesby_savejson(self):
+        text = self.do_command('chargesby', op1 = 'KO', datefrom = '20200320', dateto = '20200404', savejson = "yes")
+        if (not path.exists("tl2175app/management/commands/results/json/ChargesBy_KO_20200320_20200404.json")):
+            self.assertEqual(0,1)
+
+    def test_chargesby_csv(self):
+        text = self.do_command('chargesby', op1 = 'KO', datefrom = '20200320', dateto = '20200404', format = 'yes')
+        if (not path.exists("tl2175app/management/commands/results/csv/ChargesBy_KO_20200320_20200404.csv")):
+            self.assertEqual(0,1)
+
+    def test_chargesby_test01(self):
+        text = self.do_command('chargesby', op1 = 'KO', datefrom = '20200320', dateto = '20200404')
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.assertEqual("""{'opID': 'KO', 'RequestTimeStamp': '""" + timestamp + """', 'PeriodFrom': '2020-03-20 00:00:00', 'PeriodTo': '2020-04-04 00:00:00', 'PPOList': [{'VisitingOperator': 'AO', 'NumberOfPasses': 2, 'PassesCost': 3.0}, {'VisitingOperator': 'GF', 'NumberOfPasses': 2, 'PassesCost': 4.3}, {'VisitingOperator': 'EG', 'NumberOfPasses': 0, 'PassesCost': 0}, {'VisitingOperator': 'MR', 'NumberOfPasses': 3, 'PassesCost': 6.0}, {'VisitingOperator': 'NE', 'NumberOfPasses': 3, 'PassesCost': 6.3}, {'VisitingOperator': 'OO', 'NumberOfPasses': 2, 'PassesCost': 5.1}]}\n\n""", text)
+
+class HealthCheckTest(TestCase):
+    def do_command(self, *args, **options):
+        with StringIO() as f:
+            call_command(*args, stdout = f)
+            return f.getvalue()
     def test_healthcheck(self):
         text = self.do_command('healthcheck')
         self.assertEqual("[{'status': 'OK', 'dbconnection': 'Connected'}]\n\n", text)
+
+class ConfigurePaymentsTest(TestCase):
+    def do_command(self, *args, **options):
+        with StringIO() as f:
+            call_command(*args, **options, stdout = f)
+            return f.getvalue()
+    def test_configurepayments_wrong_dates(self):
+        text = self.do_command('configurepayments', op1 = 'KO', op2 = 'AO', datefrom = '20200404', dateto = '20190404')
+        self.assertEqual('Invalid arguments: date_from > date_to\n\n', text)
+
+    def test_configurepayments_wrong_dateformat(self):
+        text = self.do_command('configurepayments', op1 = 'KO', op2 = 'AO', datefrom = '2020/04/04', dateto = '20350404')
+        self.assertEqual('Wrong DateTime Format\n\n', text)
+    """
+    def test_configurepayments_wrong_ops(self):
+        text = self.do_command('configurepayments', op1 = 'AJ', op2 = 'AO', datefrom = '20190404', dateto = '20200404')
+        self.assertEqual('Invalid arguments: Provider does not exist\n\n', text)
+    """
+    def test_configurepayments_savejson(self):
+        self.do_command('configurepayments', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404', savejson = "yes")
+        if (not path.exists("tl2175app/management/commands/results/json/ConfigurePayments_KO_AO_20200320_20200404.json")):
+            self.assertEqual(0,1)
+    def test_configurepayments_csv(self):
+        text = self.do_command('configurepayments', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404', format = 'yes')
+        if (not path.exists("tl2175app/management/commands/results/csv/ConfigurePayments_KO_AO_20200320_20200404.csv")):
+            self.assertEqual(0,1)
+    def test_configurepayments_test01(self):
+        text = self.do_command('configurepayments', op1 = 'KO', op2 = 'AO', datefrom = '20200320', dateto = '20200404')
+        self.assertEqual("""{'operators': 'KO AO', 'cost': -13.8}\n\n""", text)
 """
 class ResetPasses(TestCase):
     def do_command(self, *args, **options):
