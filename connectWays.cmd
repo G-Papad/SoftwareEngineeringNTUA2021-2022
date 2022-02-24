16dff25fdbb8862452d95b9847d8e1fb4047d68a
G-Papad <geopapadoulis@gmail.com>
2022-02-24 10:34:23 +0200

    connectWays.cmd

--- connectWays.cmd
+++ connectWays.cmd
@@ -1,4 +1,5 @@
 call .\tl21-75-env\Scripts\activate
-start python .\tl2175\manage.py runserver
+call cd ./tl2175
+start python .\manage.py runserver
 start chrome http://127.0.0.1:8000/interoperability/api
-start cmd /k "TITLE ConnectWays_CLI"
+start cmd /k "TITLE ConnectWays_CLI" 
