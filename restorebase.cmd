90f535e0b8300560e713dbd709281d5d5083b6bc
Marina Kontalexi <kontalexim@gmail.com>
2022-02-24 02:24:07 +0200

    restorebase script

---
+++ restorebase.cmd
@@ -0,0 +1,4 @@
+call python .\tl2175\manage.py resetpasses
+call python .\tl2175\manage.py resetvehicles
+call python .\tl2175\manage.py resetstations
+call python .\tl2175\manage.py admin --passesupd --source .\tl2175\testing_data\sampledata01_passes100_8000.csv
